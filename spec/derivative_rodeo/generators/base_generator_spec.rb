ed0cc6bdaad4428ac474cce3358f42f1415a3741
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-05-04 10:31:47 -0400

    Updating base generator to leverage testing macros

--- spec/derivative_rodeo/generators/base_generator_spec.rb
+++ spec/derivative_rodeo/generators/base_generator_spec.rb
@@ -2,31 +2,33 @@
 
 RSpec.describe DerivativeRodeo::Generators::BaseGenerator do
   let(:kwargs) { { input_uris: [] } }
-  subject { described_class.new(**kwargs) }
-
-  it 'set the output adapter to same unless specified' do
-    expect(subject.output_adapter_name).to eq(DerivativeRodeo::StorageAdapters::SAME)
-  end
-
-  context 'with a specified output adapter' do
-    let(:kwargs) { { input_uris: [], output_adapter_name: 's3' } }
-
-    it 'set the output adapter to same unless specified' do
-      expect(subject.output_adapter_name).to eq('s3')
-    end
-  end
+  subject(:instance) { described_class.new(**kwargs) }
 
   %i[input_uris output_adapter_name output_extension generated_files].each do |method|
-    it "responds to #{method}" do
-      expect(subject).to respond_to(method)
+    it { is_expected.to respond_to(method) }
+    it { is_expected.to respond_to("#{method}=") }
+  end
+
+  its(:output_extension) { is_expected.to be_nil }
+
+  describe '#output_adapter_name' do
+    subject(:output_adapter_name) { instance.output_adapter_name }
+
+    context 'by default' do
+      it { is_expected.to eq(DerivativeRodeo::StorageAdapters::SAME) }
     end
 
-    it "responds to #{method}=" do
-      expect(subject).to respond_to("#{method}=")
+    context 'when given a value during instantation' do
+      let(:kwargs) { { input_uris: [], output_adapter_name: 's3' } }
+      it 'uses the given value' do
+        expect(subject).to eq('s3')
+      end
     end
   end
 
-  it 'requires build_step to be defined by a child class' do
-    expect { subject.build_step(in_file: nil, out_file: nil) }.to raise_error(NotImplementedError)
+  describe '#build_step' do
+    it 'must be defined by a child class' do
+      expect { subject.build_step(in_file: nil, out_file: nil) }.to raise_error(NotImplementedError)
+    end
   end
 end
