09b2f6123eb487f716975f27277031489bacd69d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-05-08 14:05:08 -0400

    Reworking how we convert URIs via template

--- lib/derivative_rodeo/services/convert_uris_with_uri_template_service.rb
+++ lib/derivative_rodeo/services/convert_uris_with_uri_template_service.rb
@@ -8,12 +8,22 @@ module DerivativeRodeo
     #
     # @see .call
     class ConvertUrisWithUriTemplateService
-      SCHEME_PREFIX_REGEXP = %r{^(?<scheme>\w+://)}.freeze
-      PATH_PARTS_REPLACEMENT_REGEXP = %r{\{\{ *path_parts\[(?<left>\-?\d+)\.\.(?<right>\-?\d+)\] *\}\}}.freeze
-      SCHEME_REPLACEMENT_REGEXP = %r{^\{\{ *from_scheme *\}\}}.freeze
+      DIR_PARTS_REPLACEMENT_REGEXP = %r{\{\{ *dir_parts\[(?<left>\-?\d+)\.\.(?<right>\-?\d+)\] *\}\}}.freeze
+      FILENAME_REPLACEMENT_REGEXP = %r{\{\{ *filename *\}\}}.freeze
+      BASENAME_REPLACEMENT_REGEXP = %r{\{\{ *basename *\}\}}.freeze
+      EXTENSION_REPLACEMENT_REGEXP = %r{\{\{ *extension *\}\}}.freeze
 
       ##
       # Convert the given :from_uris to a different list of uris based on the given :template.
+      #
+      # Components of the template:
+      #
+      # - basename :: the file's basename without extension
+      # - extension :: the file's extension with the period
+      # - dir_parts :: the directory parts in which the file exists; excludes the scheme
+      # - filename :: a convenience that could be represented as `basename.extension`
+      #
+      # The specs demonstrate the use cases.
       #
       # @param from_uris [Array<String>]
       # @param template [String] Another URI that may contain path_parts or scheme template values.
@@ -24,27 +34,32 @@ module DerivativeRodeo
       # @example
       #   DerivativeRodeo::Services::ConvertUrisWithUriTemplateService.call(
       #     from_uris: ["file:///path1/A/file.pdf", "file:///path2/B/file.pdf"],
-      #     template: "file:///dest1/{{path_parts[-2..-1]}}")
-      #   => ["file:///dest1/A/file.pdf", "file:///dest1/B/file.pdf"]
+      #     template: "file:///dest1/{{dir_parts[-2..-1]}}/{{filename}}")
+      #   => ["file:///dest1/path2/A/file.pdf", "file:///dest1/path2/B/file.pdf"]
       #
       #   DerivativeRodeo::Services::ConvertUrisWithUriTemplateService.call(
       #     from_uris: ["file:///path1/A/file.pdf", "aws:///path2/B/file.pdf"],
-      #     template: "{{scheme}}:///dest1/{{path_parts[-2..-1]}}")
+      #     template: "file:///dest1/{{dir_parts[-1..-1]}}/{{ filename }}")
       #   => ["file:///dest1/A/file.pdf", "aws:///dest1/B/file.pdf"]
       def self.call(from_uris:, template:, separator: "/")
         from_uris.map do |from_uri|
-          parts = from_uri.sub(SCHEME_PREFIX_REGEXP, "").split(separator)
+          _scheme, path = from_uri.split("://")
+          parts = path.split(separator)
+          dir_parts = parts[0..-2]
+          filename = parts[-1]
+          basename = File.basename(filename, ".*")
+          extension = File.extname(filename)
 
-          to_uri = template.gsub(PATH_PARTS_REPLACEMENT_REGEXP) do |text|
+          to_uri = template.gsub(DIR_PARTS_REPLACEMENT_REGEXP) do |text|
             # The yielded value does not include capture regions.  So I'm re-matching things.
             # capture region to handle this specific thing.
-            match = PATH_PARTS_REPLACEMENT_REGEXP.match(text)
-            parts[(match[:left].to_i)..(match[:right].to_i)].join(separator)
+            match = DIR_PARTS_REPLACEMENT_REGEXP.match(text)
+            dir_parts[(match[:left].to_i)..(match[:right].to_i)].join(separator)
           end
 
-          to_uri.gsub(SCHEME_REPLACEMENT_REGEXP) do |_text|
-            SCHEME_PREFIX_REGEXP.match(from_uri)[:scheme]
-          end
+          to_uri = to_uri.gsub(EXTENSION_REPLACEMENT_REGEXP, extension)
+          to_uri = to_uri.gsub(BASENAME_REPLACEMENT_REGEXP, basename)
+          to_uri.gsub(FILENAME_REPLACEMENT_REGEXP, filename)
         end
       end
     end
--- spec/derivative_rodeo/services/convert_uris_with_uri_template_service_spec.rb
+++ spec/derivative_rodeo/services/convert_uris_with_uri_template_service_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe DerivativeRodeo::Services::ConvertUrisWithUriTemplateService do
     subject { described_class.call(**kwargs) }
     [
       { from_uris: ["file:///path1/A/file.pdf", "file:///path2/B/file.pdf"],
-        template: "file:///dest1/{{path_parts[-2..-1]}}",
+        template: "file:///dest1/{{dir_parts[-1..-1]}}/{{filename}}",
         expected: ["file:///dest1/A/file.pdf", "file:///dest1/B/file.pdf"] },
-      { from_uris: ["aws:///path1/A/file.pdf", "aws:///path2/B/file.pdf"],
-        template: "{{ from_scheme }}/dest1/{{path_parts[-2..-1]}}",
-        expected: ["aws:///dest1/A/file.pdf", "aws:///dest1/B/file.pdf"] },
-      { from_uris: ["aws:///path1/A/file.pdf", "file:///path2/B/file.pdf"],
-        template: "{{from_scheme}}/dest1/{{  path_parts[-2..-1]  }}",
-        expected: ["aws:///dest1/A/file.pdf", "file:///dest1/B/file.pdf"] }
+      { from_uris: ["file:///path1/A/file1.pdf", "aws:///path2/B/file2.pdf"],
+        template: "file:///dest1/{{dir_parts[-1..-1]}}/{{filename}}",
+        expected: ["file:///dest1/A/file1.pdf", "file:///dest1/B/file2.pdf"] },
+      { from_uris: ["file:///path1/A/file1.pdf", "aws:///path2/B/file2.pdf"],
+        template: "file:///dest1/{{dir_parts[-1..-1]}}/{{basename}}/derived{{extension}}",
+        expected: ["file:///dest1/A/file1/derived.pdf", "file:///dest1/B/file2/derived.pdf"] },
     ].each do |hash|
       context "with from_uris: #{hash.fetch(:from_uris)} and template: #{hash.fetch(:template)}" do
         let(:kwargs) { hash.slice(:from_uris, :template) }
