d7707dac0153e720f62028f4d1e0e4a44c341146
Kirk Wang <kirk.wang@scientist.com>
2023-05-30 09:09:48 -0700

    🎁 Add Alto generator
    This commit will add an Alto XML generator that uses the HOCR file.  In
    the spec, we check if the XML is well-formed with nokogiri.
    
    - Ref: https://github.com/scientist-softserv/derivative_rodeo/issues/28

--- lib/derivative_rodeo/generators/alto_generator.rb
+++ lib/derivative_rodeo/generators/alto_generator.rb
@@ -5,11 +5,11 @@ require_relative '../services/extract_word_coordinates_from_hocr_sgml_service'
 module DerivativeRodeo
   module Generators
     ##
-    # Generate the word coordinates (as JSON) from the given input_uris.
+    # Generate the Alto XML from the given input_uris.
     #
     # @note Assumes that we're receiving a HOCR file (generated via {HocrGenerator}).
-    class PlainTextGenerator < BaseGenerator
-      self.output_extension = "plain_text.txt"
+    class AltoGenerator < BaseGenerator
+      self.output_extension = "alto.xml"
 
       class_attribute :service, default: Services::ExtractWordCoordinatesFromHocrSgmlService
 
@@ -22,7 +22,7 @@ module DerivativeRodeo
       # @see #requisite_files
       def build_step(output_location:, input_tmp_file_path:, **)
         output_location.with_new_tmp_path do |output_tmp_file_path|
-          convert_to_coordinates(path_to_hocr: input_tmp_file_path, path_to_plain_text: output_tmp_file_path)
+          convert_to_coordinates(path_to_hocr: input_tmp_file_path, path_to_alto: output_tmp_file_path)
         end
       end
 
@@ -30,11 +30,11 @@ module DerivativeRodeo
 
       ##
       # @param path_to_hocr [String]
-      # @param path_to_plain_text [String]
-      def convert_to_coordinates(path_to_hocr:, path_to_plain_text:)
+      # @param path_to_alto [String]
+      def convert_to_coordinates(path_to_hocr:, path_to_alto:)
         hocr_html = File.read(path_to_hocr)
-        File.open(path_to_plain_text, "w+") do |file|
-          file.puts service.call(hocr_html).to_text
+        File.open(path_to_alto, "w+") do |file|
+          file.puts service.call(hocr_html).to_alto
         end
       end
     end
