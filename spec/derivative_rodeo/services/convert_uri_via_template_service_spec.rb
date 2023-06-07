9bb576bb7f6017e451f36f11aed8550bed447c30
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-06-07 17:07:55 -0400

    🐛 Handling "SAME" output extension for templates (#51)
    Prior to this commit, we were seeing "filename.same" in SpaceStone
    deploys; that's not a likely extension but instead reflects our "same"
    convention, namely the output extension of a derivative is the same as
    the given input.

--- spec/derivative_rodeo/services/convert_uri_via_template_service_spec.rb
+++ spec/derivative_rodeo/services/convert_uri_via_template_service_spec.rb
@@ -22,7 +22,12 @@ RSpec.describe DerivativeRodeo::Services::ConvertUriViaTemplateService do
         template: "file:///dest1/{{dir_parts[-1..-1]}}/{{basename}}/derived{{extension}}",
         extension: "hello",
         adapter: DerivativeRodeo::StorageLocations::FileLocation,
-        expected: "file:///dest1/A/file1/derived.hello" }
+        expected: "file:///dest1/A/file1/derived.hello" },
+      { from_uri: "file:///path1/A/file1.pdf",
+        template: "file:///dest1/{{dir_parts[-1..-1]}}/{{basename}}/derived{{extension}}",
+        extension: DerivativeRodeo::StorageLocations::SAME,
+        adapter: DerivativeRodeo::StorageLocations::FileLocation,
+        expected: "file:///dest1/A/file1/derived.pdf" }
     ].each do |hash|
       context "with #{hash.except(:expected)}" do
         let(:kwargs) { hash.except(:expected) }
